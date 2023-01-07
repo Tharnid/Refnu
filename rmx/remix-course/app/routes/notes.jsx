978344bf04ae8e0ec6442ec7f30b9410af4d51b5
Tharnid <jeffmsmithdev@gmail.com>
2023-01-07 17:15:36 -0500

    adding and displaying all notes

--- rmx/remix-course/app/routes/notes.jsx
+++ rmx/remix-course/app/routes/notes.jsx
@@ -1,17 +1,49 @@
-// import NewNote from '~/components/NewNote';
-// import newNotesStyles from '~/components/NewNote.css';
-
+import { redirect } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 import NewNote, { links as newNoteLinks } from "~/components/NewNote";
+import NoteList, {links as noteListLinks} from "~/components/NoteList";
+import { getStoredNotes, storeNotes } from "~/data/notes";
 // surfacing links
 
 export default function NotesPage() {
-    return (
+    // load data hook from remix
+    const notes = useLoaderData();
+
+
+    return ( // now passing notes
     <main>
         <NewNote />
+        <NoteList notes={notes} /> 
     </main>
     );
-} 
+}
 
+// Load data when component is loaded when a GET request comes through
+export async function loader() {
+    const notes = await getStoredNotes();
+    return notes;  // plain data
+}
+
+// actions?
+export async function action({request}) {
+    const formData = await request.formData();
+    const noteData = Object.fromEntries(formData);
+    // Add validation
+    
+    // existing notes
+    const existingNotes = await getStoredNotes();
+    noteData.id = new Date().toISOString();
+
+    // updated notes
+    const updatedNotes = existingNotes.concat(noteData);
+
+    // store updated notes in updated notes.json
+    await storeNotes(updatedNotes);
+
+    // return response
+    return redirect('/notes');
+
+}
 export function links() {
-    return [...newNoteLinks()];
+    return [...newNoteLinks(), ...noteListLinks()]; // spread new note and noteListLinks
 }
